fd92defeb254f6f46b468de97976ff0c5df87786
Rui Pinheiro <rpinheiro@neoception.com>
2022-11-29 19:27:54 +0000

    build-a-bot: fix formatting

--- build-a-bot/src/build/PartSelector.vue
+++ build-a-bot/src/build/PartSelector.vue
@@ -61,6 +61,7 @@ export default {
   height: 165px;
   border: 3px solid #aaa;
 }
+
 .sale {
   position: absolute;
   bottom: 5px;
@@ -69,6 +70,7 @@ export default {
   background-color: red;
   padding: 3px;
 }
+
 .part-title {
   position: absolute;
   width: 100%;
@@ -78,27 +80,35 @@ export default {
   padding-top: 5px;
   top: -25px;
 }
+
 .part img {
   width: 165px;
 }
+
 .top {
   border-bottom: none;
 }
+
 .left {
   border-right: none;
 }
+
 .right {
   border-left: none;
 }
+
 .bottom {
   border-top: none;
 }
+
 .left img {
   transform: rotate(-90deg);
 }
+
 .right img {
   transform: rotate(90deg);
 }
+
 .prev-selector {
   position: absolute;
   z-index: 1;
@@ -107,6 +117,7 @@ export default {
   width: 25px;
   height: 171px;
 }
+
 .next-selector {
   position: absolute;
   z-index: 1;
@@ -115,34 +126,41 @@ export default {
   width: 25px;
   height: 171px;
 }
+
 .left .prev-selector:after,
 .right .prev-selector:after {
   content: "\25B2";
 }
+
 .left .next-selector:after,
 .right .next-selector:after {
   content: "\25BC";
 }
+
 .top .prev-selector:after,
 .bottom .prev-selector:after,
 .center .prev-selector:after {
   content: "\25C4";
 }
+
 .top .next-selector:after,
 .bottom .next-selector:after,
 .center .next-selector:after {
   content: "\25BA";
 }
+
 .center .prev-selector,
 .center .next-selector {
   opacity: 0.8;
 }
+
 .left .prev-selector {
   top: -28px;
   left: -3px;
   width: 144px;
   height: 25px;
 }
+
 .left .next-selector {
   top: auto;
   bottom: -28px;
@@ -150,12 +168,14 @@ export default {
   width: 144px;
   height: 25px;
 }
+
 .right .prev-selector {
   top: -28px;
   left: 24px;
   width: 144px;
   height: 25px;
 }
+
 .right .next-selector {
   top: auto;
   bottom: -28px;
@@ -163,6 +183,7 @@ export default {
   width: 144px;
   height: 25px;
 }
+
 .highlight {
   border: 1px solid red;
 }
