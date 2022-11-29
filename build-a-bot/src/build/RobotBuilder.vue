63daad41dbdfc7347824c1097b5540691fcbf3b8
Rui Pinheiro <rpinheiro@neoception.com>
2022-11-29 11:03:58 +0000

    build-a-bot: use v-for to show a shopping cart item list

--- build-a-bot/src/build/RobotBuilder.vue
+++ build-a-bot/src/build/RobotBuilder.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="content">
+    <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
     <div class="top-row">
       <div class="top part">
         <div class="robot-name">
@@ -51,6 +52,23 @@
         <button @click="selectNextBase()" class="next-selector">&#9658;</button>
       </div>
     </div>
+    <div>
+      <h1>Cart</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Robot</th>
+            <th class="cost">Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(robot, index) in cart" :key="index">
+            <td>{{ robot.head.title }}</td>
+            <td class="cost">{{ robot.cost }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 <script>
@@ -71,6 +89,7 @@ export default {
   data() {
     return {
       availableParts,
+      cart: [],
       selectNextHeadIndex: 0,
       selectNextLeftArmIndex: 0,
       selectNextTorsoIndex: 0,
@@ -90,6 +109,16 @@ export default {
     },
   },
   methods: {
+    addToCart() {
+      const robot = this.selectedRobot;
+      const cost =
+        robot.head.cost +
+        robot.leftArm.cost +
+        robot.torso.cost +
+        robot.rightArm.cost +
+        robot.base.cost;
+      this.cart.push(Object.assign({}, robot, { cost }));
+    },
     selectNextHead() {
       this.selectNextHeadIndex = getNextValidIndex(
         this.selectNextHeadIndex,
@@ -252,5 +281,24 @@ export default {
 }
 .sale {
   color: red;
+}
+.content {
+  position: relative;
+}
+.add-to-cart {
+  position: absolute;
+  right: 30px;
+  width: 220px;
+  padding: 3px;
+  font-size: 16px;
+}
+td,
+th {
+  text-align: left;
+  padding: 5px;
+  padding-right: 20px;
+}
+.cost {
+  text-align: right;
 }
 </style>
