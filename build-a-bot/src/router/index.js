cc8b0425975e76b2972504da596b11f93c898efa
Rui Pinheiro <rpinheiro@neoception.com>
2022-12-02 10:56:59 +0000

    build-a-bot: use Nested Routes to present sub-pages in tabbed way

--- build-a-bot/src/router/index.js
+++ build-a-bot/src/router/index.js
@@ -4,6 +4,11 @@ import HomePage from "../home/HomePage.vue";
 import RobotBuilder from "../build/RobotBuilder.vue";
 import PartInfo from "../parts/PartInfo.vue";
 import Search from "../search/Search.vue";
+import BrowseParts from "../parts/BrowseParts.vue";
+import RobotHeads from "../parts/RobotHeads.vue";
+import RobotArms from "../parts/RobotArms.vue";
+import RobotTorsos from "../parts/RobotTorsos.vue";
+import RobotBases from "../parts/RobotBases.vue";
 
 export default createRouter({
   history: createWebHashHistory(),
@@ -17,6 +22,23 @@ export default createRouter({
       path: "/build",
       name: "Build",
       component: RobotBuilder,
+    },
+    {
+      // Beginner note: this must go before the /parts/:partType/:id
+      //                route, otherwise /parts/browse will match it.
+      //                (:partType = "browse" and :id = empty)
+      //                Order in routing is important: if a URL matches
+      //                multiple route patterns, the topmost one will
+      //                be the one followed.
+      path: "/parts/browse",
+      name: "BrowseParts",
+      component: BrowseParts,
+      children: [
+        { name: "BrowseHeads", path: "heads", component: RobotHeads },
+        { name: "BrowseArms", path: "arms", component: RobotArms },
+        { name: "BrowseTorsos", path: "torsos", component: RobotTorsos },
+        { name: "BrowseBases", path: "bases", component: RobotBases },
+      ],
     },
     {
       path: "/parts/:partType/:id",
