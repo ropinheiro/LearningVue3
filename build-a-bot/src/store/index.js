7462d31fc2f836b63a8151e16a645cf4d901ad0e
Rui Pinheiro <rpinheiro@neoception.com>
2022-12-03 20:09:12 +0000

    build-a-bot: add a second users modules to the store

--- build-a-bot/src/store/index.js
+++ build-a-bot/src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from "vuex";
 import robotsModule from "./modules/robots";
+import usersModule from "./modules/users";
 
 export default createStore({
   modules: {
     robots: robotsModule,
+    users: usersModule,
   },
 });
