b6c235354e5b3d14c7a290b59f2910fcfed36ae6
Rui Pinheiro <rpinheiro@neoception.com>
2022-12-03 19:37:38 +0000

    build-a-bot: fix typo in a console log text

--- build-a-bot/src/search/usePagination.js
+++ build-a-bot/src/search/usePagination.js
@@ -10,7 +10,7 @@ export default function usePagination(filteredSearchResults) {
     currentPage.value -= 1;
   };
 
-  onMounted(() => console.log("Mounted: useSearch"));
+  onMounted(() => console.log("Mounted: usePagination"));
 
   const currentStartIndex = computed(
     () => (currentPage.value - 1) * pageSize + 1
