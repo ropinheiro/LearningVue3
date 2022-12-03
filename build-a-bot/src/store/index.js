a1e6c083a436f00a070c190e66f19bbd1bcdcbf4
Rui Pinheiro <rpinheiro@neoception.com>
2022-12-03 19:48:33 +0000

    build-a-bot: example doing a POST using Vuex actions

--- build-a-bot/src/store/index.js
+++ build-a-bot/src/store/index.js
@@ -27,6 +27,13 @@ export default createStore({
         .then((result) => commit("updateParts", result.data))
         .catch(console.error);
     },
+    addRobotToCart({ commit, state }, robot) {
+      const cart = [...state.cart, robot];
+      axios
+        .post("/api/cart", cart)
+        .then(() => commit("addRobotToCart", robot))
+        .catch(console.error);
+    },
   },
   getters: {
     cartSaleItems(state) {
