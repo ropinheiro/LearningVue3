6abb0d3d3cedbfa13c5a5960c4330f68c05f2b24
Rui Pinheiro <rpinheiro@neoception.com>
2022-12-04 13:02:11 +0000

    build-a-bot: code custom directive in a more flexible way

--- build-a-bot/src/build/PartSelector.vue
+++ build-a-bot/src/build/PartSelector.vue
@@ -3,7 +3,12 @@
     <img @click="showPartInfo()" :src="selectedPart.src" title="arm" />
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span v-pin class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span
+      v-pin="{ bottom: pinPadding, right: pinPadding }"
+      class="sale"
+      v-show="selectedPart.onSale"
+      >Sale!</span
+    >
   </div>
 </template>
 
@@ -32,7 +37,7 @@ export default {
     },
   },
   data() {
-    return { selectedPartIndex: 0 };
+    return { selectedPartIndex: 0, pinPadding: "10px" };
   },
   computed: {
     selectedPart() {
--- build-a-bot/src/shared/pin-directive.js
+++ build-a-bot/src/shared/pin-directive.js
@@ -1,7 +1,25 @@
+export default function (element, binding) {
+  Object.keys(binding.value).forEach((position) => {
+    element.style[position] = binding.value[position];
+  });
+  element.style.position = "absolute";
+}
+
+// Beginner note: doing the directive the way shown above instead
+// of the way shown below (done previously) allows the directive
+// to react to both "beforeMount" and "updated" in a less cerimony
+
+/*
 export default {
   beforeMount(element) {
     element.style.position = "absolute";
     element.style.bottom = "5px";
     element.style.right = "5px";
   },
+  updated(element) {
+    element.style.position = "absolute";
+    element.style.bottom = "5px";
+    element.style.right = "5px";
+  },
 };
+*/
